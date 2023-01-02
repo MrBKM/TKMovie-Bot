0e0b88055f6df376e537b585235c5238bd523577
BKM TG <119480777+MrBKM@users.noreply.github.com>
2023-01-02 07:51:21 +0530

    Update json.py

--- plugins/json.py
+++ plugins/json.py
@@ -1,23 +1,8 @@
-#!/usr/bin/env python3
-# Copyright (C) @ZauteKm
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 import os
 from pyrogram import Client, filters, enums 
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
 
-@Client.on_message(filters.command(["json", 'js', 'showjson']))
+@Client.on_message(filters.command(["json"]))
 async def jsonify(_, message):
     the_real_message = None
     reply_to_id = None
@@ -32,7 +17,7 @@ async def jsonify(_, message):
                 [
                     InlineKeyboardButton(
                         text="✗ close ✗",
-                        callback_data="close_data"
+                        callback_data="close"
                     )
                 ]
             ]
@@ -46,7 +31,7 @@ async def jsonify(_, message):
                 [
                     InlineKeyboardButton(
                         text="✗ close ✗",
-                        callback_data="close_data"
+                        callback_data="close"
                     )
                 ]
             ]
