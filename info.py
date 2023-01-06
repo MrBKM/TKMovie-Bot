eafd05d447c6f05c9e8ca5399dd275906b23a71f
BKM TG <119480777+MrBKM@users.noreply.github.com>
2023-01-06 19:58:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -23,9 +23,9 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 PICS = (environ.get('PICS', 'https://telegra.ph/file/3b990c107bec4568bb7b1.jpg')).split()
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5970680890 953377581').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5970680890 953377581 5925484131').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001576725431').split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '5970680890 953377581').split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '5970680890 953377581 5925484131').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL', '-1001506877410')
 auth_grp = environ.get('AUTH_GROUP')
